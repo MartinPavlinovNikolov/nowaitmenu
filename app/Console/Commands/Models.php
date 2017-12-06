35d148386bebde6285c7d4703a8059a6403e612d
Martin_Nikolov <martin_nikolov.89@abv.bg>
2017-12-07 01:32:59 +0200

    addet new command to artisan: models

---
+++ app/Console/Commands/Models.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Models extends Command
+{
+
+    private $models_strtolower = [];
+    private $models_ucfirst    = [];
+    private $tableWidth        = 0;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'models{--r|relationships}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show info for models';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * set all models from default location "app\"
+     * 
+     * @return $this
+     */
+    private function setAllModels()
+    {
+        $models = array_filter(glob(app_path() . DIRECTORY_SEPARATOR . '*'), 'is_file');
+        foreach ($models as $m)
+        {
+            $m                         = explode('\\', $m);
+            $m                         = end($m);
+            $m                         = str_replace('.php', '', $m);
+            $this->tableWidth          = $this->tableWidth <= strlen($m) ? strlen($m) : $this->tableWidth;
+            $this->models_ucfirst[]    = ucfirst($m);
+            $this->models_strtolower[] = strtolower($m);
+        }
+
+        return $this;
+    }
+
+    private function dashes()
+    {
+        $dashes = '+----------';
+        for($i=0;$i<=$this->tableWidth;$i++){
+            $dashes .= '-';
+        }
+        $dashes .= '+';
+        
+        return $dashes;
+    }
+    
+    private function generateViewTable()
+    {
+        $this->info("\n");
+        $this->info($this->dashes());
+        $this->info('|     models');
+        $this->info($this->dashes());
+        $i = 0;
+        foreach ($this->models_ucfirst as $m)
+        {
+            $this->info('|');
+            $this->info('|        ' . $m);
+            $i++;
+        }
+        $this->info("|");
+        $this->info($this->dashes());
+        $this->info("|  total models: " . $i);
+        $this->info($this->dashes());
+        $this->info("\n");
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->setAllModels();
+        $this->generateViewTable();
+    }
+
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-            //
+        Commands\Models::class
     ];
 
     /**
@@ -39,64 +39,6 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
-        $this->command('model:list-r', function() {
-            $models     = array_filter(glob(app_path() . DIRECTORY_SEPARATOR . '*'), 'is_file');
-            $arrModels  = [];
-            $arrModels2 = [];
-            foreach ($models as $k => $m)
-            {
-                $m              = explode(DIRECTORY_SEPARATOR, $m);
-                $m              = end($m);
-                $str_m          = str_replace('.php', '', $m);
-                $arrModels[$k]  = $str_m;
-                $arrModels2[$k] = $str_m;
-            }
-
-            $this->info("\n");
-
-            $arr_methods = [];
-            foreach ($arrModels2 as $m)
-            {
-                $arr_methods[] = strtolower($m);
-                $arr_methods[] = strtolower(str_plural($m));
-            }
-            foreach ($arrModels as $model)
-            {
-                $this->info('Model: ' . $model);
-                foreach ($arr_methods as $method_name)
-                {
-                    if (method_exists('App\\' . $model, $method_name)) {
-                        if (substr($method_name, -1 == 's') && substr($method_name, -2) != 's') {
-                            $method_name = rtrim($method_name, 's');
-                        }
-                        $method = ucfirst($method_name);
-                        $this->info('          |      ______________');
-                        $this->info('          |_____| ' . $method);
-                        $this->info('                |______________');
-                    }
-                }
-
-                $this->info("\n");
-            }
-        });
-        
-        $this->command('model:list', function() {
-            $models     = array_filter(glob(app_path() . DIRECTORY_SEPARATOR . '*'), 'is_file');
-            $this->info("\n");
-            $i = 0;
-            foreach ($models as $k => $m)
-            {
-                $m              = explode(DIRECTORY_SEPARATOR, $m);
-                $m              = end($m);
-                $str_m          = str_replace('.php', '', $m);
-                $this->info('    ' .$str_m);
-                $i++;
-            }
-            $this->info("_________________");
-            $this->info("total models: " . $i);
-            $this->info("_________________");
-            $this->info("\n");
-        });
     }
 
 }
