fe2aeb4b1d3c954c892dd5cad4dbba990b71db6c
Martin_Nikolov <martin_nikolov.89@abv.bg>
2017-12-25 18:50:33 +0200

    start learning testing

---
+++ tests/Feature/MarketingPageTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MarketingPageTest extends TestCase
+{
+    /** 
+     * @test
+     */
+    public function can_browser_get_responce_200_when_visit_marketing_page()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+    
+    /**
+     * @test
+     */
+    public function can_user_see_correct_text_in_the_marketing_page()
+    {
+        $response = $this->get('/');
+        $response->assertSee('NoWaitMenu')
+        ->assertSee('try it free')
+        ->assertSee('Features')
+        ->assertSee('Pricing')
+        ->assertSee('Support')
+        ->assertSee('Register')
+        ->assertSee('Login')
+        ->assertSee('Get your restaurant runing in minutes');
+    }
+}
